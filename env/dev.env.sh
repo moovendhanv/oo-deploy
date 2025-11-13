a47cb86b530e03b410f4812e942c45cedea7827f
Anthony Akash <anthonyakash@Thirumarans-MacBook-Air-2.local>
2025-11-13 15:05:32 +0530

    Changes in dev

--- env/dev.env.sh
+++
@@ -1,6 +0,0 @@
-# Parameters
-STACK_NAME="batch-spot-stack-test"
-VPC_ID="vpc-090bdd5202004454d"
-SUBNET_IDS="subnet-0cbca799834652c91 subnet-092b13bb6362e51c3"
-REGION="us-east-1"
-ENVIRONMENT="dev"
